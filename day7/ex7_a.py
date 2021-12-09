42787acf576041494294966b79c24de3b9e0d015
Marta <46792989+MartaRudzite@users.noreply.github.com>
2021-12-09 23:49:20 +0000

    Add files via upload

--- day7/ex7_a.py
+++ day7/ex7_a.py
@@ -1,7 +1,7 @@
 import copy
 
-# in_file = 'd7_test_in.txt'
-in_file = 'd7_in.txt'
+in_file = 'd7_test_in.txt'
+# in_file = 'd7_in.txt'
 data = [line.rstrip('\n').split(',') for line in open(in_file)]
 # print(data)
 data = [int(i) for i in data[0]]
@@ -11,7 +11,7 @@ for i in range(min(data),max(data)):
     for pos in data:
         sum += abs(i-pos)
     if sum < min_sum:
-        print(sum,min_sum)
+        print(i,sum,min_sum)
         min_sum = copy.deepcopy(sum)
         print(min_sum)
 
