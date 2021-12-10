3984e765e3e9e10356a311d9a73d18a5eb8bedf8
Marta <46792989+MartaRudzite@users.noreply.github.com>
2021-12-10 00:05:41 +0000

    Add files via upload

--- day7/ex7_b.py
+++ day7/ex7_b.py
@@ -5,14 +5,16 @@ in_file = 'd7_test_in.txt'
 data = [line.rstrip('\n').split(',') for line in open(in_file)]
 # print(data)
 data = [int(i) for i in data[0]]
+
 min_sum = 1000000000
 for i in range(min(data),max(data)):
     sum = 0
     for pos in data:
-        sum += abs(i-pos)
+        dist = abs(i-pos)
+        sum += (dist*(dist+1))/2
     if sum < min_sum:
         # print(i,sum,min_sum)
         min_sum = copy.deepcopy(sum)
         # print(min_sum)
 
-print(min_sum)
+print(int(min_sum))
